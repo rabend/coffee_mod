c3ea9be58654ba9dcd4fcfda420a06874a981c97
Roman Abendroth <r.abendroth@tarent.de>
2017-04-05 11:29:50 +0200

    added label to form

--- src/components/CoffeeForm.js
+++ src/components/CoffeeForm.js
@@ -9,31 +9,40 @@ export default class CoffeeForm extends React.Component {
         this.state = {
             coffeeChoices: ["Coffee", "Latte"],
             strengthChoices: [1, 2, 3, 4, 5],
-            selectedCoffee:"Standard",
-            selectedStrength:"Standard",
+            selectedCoffee: "Standard",
+            selectedStrength: "Standard",
         };
 
         this.handleSubmit = this.handleSubmit.bind(this);
     }
+
     handleCoffeeChange(event) {
         this.setState({selectedCoffee: event.target.value});
     }
+
     handleStrengthChanged(event) {
         this.setState({selectedStrength: event.target.value});
     }
+
     sendCoffeeSetup() {
+        alert("your configuration has been saved!");
         //AJAX POST call to backend here
     }
+
     handleSubmit(event) {
         event.preventDefault();
     }
+
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <CoffeeSelector values={this.state.coffeeChoices} onChange={this.handleCoffeeChange.bind(this)}/>
-                <StrengthSelector values={this.state.strengthChoices} onChange={this.handleStrengthChanged.bind(this)}/>
-                <input type="submit"/>
-            </form>
+            <div>
+                <label>Select your personal coffee configuration:</label>
+                <form onSubmit={this.handleSubmit}>
+                    <CoffeeSelector values={this.state.coffeeChoices} onChange={this.handleCoffeeChange.bind(this)}/>
+                    <StrengthSelector values={this.state.strengthChoices} onChange={this.handleStrengthChanged.bind(this)}/>
+                    <input type="submit" value="Send it!"/>
+                </form>
+            </div>
         )
     }
 }
