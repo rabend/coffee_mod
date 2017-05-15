f031083f76267caf2ed7c2f6839d0c3b19547e14
Roman Abendroth <r.abendroth@tarent.de>
2017-05-15 10:59:04 +0200

    changed python scripts for input read testing

--- raspberry/listenForToken.py
+++ raspberry/listenForToken.py
@@ -1,9 +1,22 @@
 import getUserConfigFromServer
+import makeCoffee
+
 
 def main():
     while True:
-        tokenHash = raw_input('Scan your token')
-        print tokenHash
-        getUserConfigFromServer.main(tokenHash)
+        try:
+            tokenHash = raw_input('Scan your token')
+            makeCoffee.main(100, 100, tokenHash)
+            #getUserConfigFromServer.main(tokenHash)
+        except:
+            print "wait a moment"
+
 if __name__ == '__main__':
     main()
+
+
+#0871097668 #1
+#3067209429 #2
+#3066946645 #3
+
+
--- raspberry/makeCoffee.py
+++ raspberry/makeCoffee.py
@@ -1,6 +1,6 @@
 import sys
-# import RPi.GPIO as GPIO
-# import time
+import RPi.GPIO as GPIO
+import time
 
 def main(coffeeamount, milkamount, strength):
     if coffeeamount == 0 or strength == 0:
@@ -8,13 +8,30 @@ def main(coffeeamount, milkamount, strength):
     print coffeeamount
     print milkamount
     print strength
+
+    scancodes = {
+
+    }
     # set RaspberryPi pin layout
-    # GPIO.setmode(GPIO.BOARD)
+    GPIO.setmode(GPIO.BOARD)
 
     # set pins for output
     # GPIO.setup(PIN_NUMBER, GPIO.OUT)
 
-
+    if strength == 0871097668:
+        GPIO.output(12, GPIO.HIGH)
+        time.sleep(3)
+        GPIO.output(12, GPIO.LOW)
+    elif strength == 3067209429:
+        GPIO.output(20, GPIO.HIGH)
+        time.sleep(3)
+        GPIO.output(12, GPIO.LOW)
+    elif strength == 3066946645:
+        GPIO.output(21, GPIO.HIGH)
+        time.sleep(3)
+        GPIO.output(12, GPIO.LOW)
+    else:
+        print "token not recognized"
     # map coffeeAmount, milkAmount and strength to a time duration
     # since we have only the option of turning the machine's parts on and off
     # we have to find a time of coffee grinding that resolves to strength 1 and then can multiply
