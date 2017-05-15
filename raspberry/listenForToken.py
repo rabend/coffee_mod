694ab399291a4340ae3b01c7317a7587a07418a8
Roman Abendroth <r.abendroth@tarent.de>
2017-05-15 13:12:41 +0200

    moved key listener to correct script

--- raspberry/listenForToken.py
+++ raspberry/listenForToken.py
@@ -1,22 +1,52 @@
 import getUserConfigFromServer
 import makeCoffee
-
+from evdev import InputDevice, categorize, ecodes
+import pythoncom, pyHook
 
 def main():
+    scancodes = {
+        0: None, 1: u'ESC', 2: u'1', 3: u'2', 4: u'3', 5: u'4', 6: u'5', 7: u'6', 8: u'7', 9: u'8', 10: u'9', 11: u'0', 12: u'-', 13: u'=',
+        14: u'BACKSPACE',
+        15: u'TAB', 16: u'Q', 17: u'W', 18: u'E', 19: u'R', 20: u'T', 21: u'Y', 22: u'U', 23: u'I', 24: u'O', 25: u'P', 26: u'(', 27: u')',
+        28: u'ENTER',
+        29: u'LEFTCTRL', 30: u'A', 31: u'S', 32: u'D', 33: u'F', 34: u'G', 35: u'H', 36: u'J', 37: u'K', 38: u'L', 39: u'SEMICOLON',
+        40: u'APOSTROPHE', 41: u'UNKOWN',
+        42: u'LEFTSHIFT', 43: u'BACKSLASH', 44: u'Z', 45: u'X', 46: u'C', 47: u'V', 48: u'B', 49: u'N', 50: u'M', 51: u'COMMA', 52: u'DOT',
+        53: u'SLASH', 54: u'RIGHTSHIFT'
+    }
+
+    device = InputDevice("/dev/input/event0")
+
+    tokenHash = ""
+
     while True:
         try:
-            tokenHash = raw_input('Scan your token')
+            for event in device.read_loop():
+                if event.type == ecodes.EV_KEY:
+                    data = categorize(event)
+                    if data.keystate == 1:
+                        key_lookup = scancodes.get(data.scancode) or u'UNKONWN: {}'.format(data.scancode)
+                        if key_lookup == u'ENTER':
+                            print tokenHash
+                            break
+                        else:
+                            tokenHash += str(key_lookup)
+            hm = pyHook.HookManager()
+            hm.KeyAll = keyLock
+            pythoncom.PumpMessages()
             makeCoffee.main(100, 100, tokenHash)
+            hm.KeyAll = keyUnlock
+            pythoncom.PumpMessages()
+            tokenHash = ""
             #getUserConfigFromServer.main(tokenHash)
         except:
             print "wait a moment"
 
+def keyLock(event):
+    return False
+
+def keyUnlock(event):
+    return True
+
 if __name__ == '__main__':
     main()
-
-
-#0871097668 #1
-#3067209429 #2
-#3066946645 #3
-
-
--- raspberry/makeCoffee.py
+++ raspberry/makeCoffee.py
@@ -11,50 +11,28 @@ def main(coffeeamount, milkamount, strength):
     # print(milkamount)
     # print(strength)
 
-    scancodes = {
-        0: None, 1: u'ESC', 2: u'1', 3: u'2', 4: u'3', 5: u'4', 6: u'5', 7: u'6', 8: u'7', 9: u'8', 10: u'9', 11: u'0', 12: u'-', 13: u'=',
-        14: u'BACKSPACE',
-        15: u'TAB', 16: u'Q', 17: u'W', 18: u'E', 19: u'R', 20: u'T', 21: u'Y', 22: u'U', 23: u'I', 24: u'O', 25: u'P', 26: u'(', 27: u')',
-        28: u'ENTER',
-        29: u'LEFTCTRL', 30: u'A', 31: u'S', 32: u'D', 33: u'F', 34: u'G', 35: u'H', 36: u'J', 37: u'K', 38: u'L', 39: u'SEMICOLON',
-        40: u'APOSTROPHE', 41: u'UNKOWN',
-        42: u'LEFTSHIFT', 43: u'BACKSLASH', 44: u'Z', 45: u'X', 46: u'C', 47: u'V', 48: u'B', 49: u'N', 50: u'M', 51: u'COMMA', 52: u'DOT',
-        53: u'SLASH', 54: u'RIGHTSHIFT'
-    }
-
     device = InputDevice("/dev/input/event0")
     # set RaspberryPi pin layout
-    GPIO.setmode(GPIO.BOARD)  # set pins for output
-    #GPIO.setup(PIN_NUMBER, GPIO.OUT)
+    GPIO.setmode(GPIO.BCM)  # set pins for output
+    GPIO.setup(12, GPIO.OUT)
+    GPIO.setup(20, GPIO.OUT)
+    GPIO.setup(21, GPIO.OUT)
 
-    print(device)
-    tokenHash = ""
-
-    for event in device.read_loop():
-        if event.type == ecodes.EV_KEY:
-            data = categorize(event)
-            if data.keystate == 1:
-                key_lookup = scancodes.get(data.scancode) or u'UNKONWN: {}'.format(data.scancode)
-                if key_lookup == u'ENTER':
-                    print tokenHash
-                    if tokenHash == "0871097668":
-                        GPIO.output(12, GPIO.HIGH)
-                        time.sleep(3)
-                        GPIO.output(12, GPIO.LOW)
-                    elif tokenHash == "3067209429":
-                        GPIO.output(20, GPIO.HIGH)
-                        time.sleep(3)
-                        GPIO.output(12, GPIO.LOW)
-                    elif tokenHash == "3066946645":
-                        GPIO.output(21, GPIO.HIGH)
-                        time.sleep(3)
-                        GPIO.output(12, GPIO.LOW)
-                    else:
-                        print "token not recognized"
-
-                    tokenHash = ""
-                else:
-                    tokenHash += str(key_lookup)
+    print strength
+    if strength == "0871097668":
+        GPIO.output(12, GPIO.HIGH)
+        time.sleep(3)
+        GPIO.output(12, GPIO.LOW)
+    elif strength == "3067209429":
+        GPIO.output(20, GPIO.HIGH)
+        time.sleep(3)
+        GPIO.output(12, GPIO.LOW)
+    elif strength == "3066946645":
+        GPIO.output(21, GPIO.HIGH)
+        time.sleep(3)
+        GPIO.output(12, GPIO.LOW)
+    else:
+        print "token not recognized"
 
 # map coffeeAmount, milkAmount and strength to a time duration
 # since we have only the option of turning the machine's parts on and off
